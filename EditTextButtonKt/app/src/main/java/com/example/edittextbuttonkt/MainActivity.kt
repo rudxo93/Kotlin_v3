0a358cefaca769388adcd22ee5c62eeefaf78e22
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-04-14 11:43:44 +0900

    EditText, button / button을 이용해서 EditText에 입력된 String으로 TextView의 값 변경하기

--- EditTextButtonKt/app/src/main/java/com/example/edittextbuttonkt/MainActivity.kt
+++ EditTextButtonKt/app/src/main/java/com/example/edittextbuttonkt/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.edittextbuttonkt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.edittextbuttonkt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    private var mBinding : ActivityMainBinding? = null
+
+    private val binding get() = mBinding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) { // activity가 최초 실행되면 이곳을 수행한다.
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.btnGetText.setOnClickListener{ // EditText에 입력되어있는 값을 가지고 와서 textView에 뿌려준다.
+            // EditText에 입력되어 text값을 String형태로 변환해서 가져온다. EditText에 입력되어 있는 값
+            var resultText = binding.etText.text.toString()
+            // 입력된 값을 textView에 set해준다.
+            binding.tvResult.setText(resultText)
+
+        }
+
+
+
     }
 }
