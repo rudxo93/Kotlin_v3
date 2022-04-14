74eab898c9e640be383e4b5f330e9489d50df06d
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-04-14 16:05:22 +0900

    Intent를 활용한 main의 데이터 Sub로 넘기기

--- IntentKt/app/src/main/java/com/example/intentkt/MainActivity.kt
+++ IntentKt/app/src/main/java/com/example/intentkt/MainActivity.kt
@@ -22,7 +22,13 @@ class MainActivity : AppCompatActivity() {
             // var : 변수 / val : 자바에서는 final 값이 변경되지 못하는 변수(상수)
             val intent = Intent(this,SubActivity::class.java) // 다음 화면으로 이동하기 위한 Intent객체 생성
 
-            startActivity(intent)
+            // intent로 이동할 때 intent라는 객체 내부에 어떠한 값을 담아서 넘겨줄 수 있다.
+            // putExtra(특정 값을 넘기기전에 그 값을 구분할 만한 key값, 실제로 넘길 객체)
+            intent.putExtra("msg", binding.tvSendMsg.text.toString()) // HelloWorld라는 텍스트 값을 담은 뒤 msg라는 키로 잠궜다.
+
+            startActivity(intent) // intent에 저장되어있는 엑티비티 쪽으로 이동한다.
+
+            finish() // 자기 자신 엑티비티를 파괴한다.
         }
     }
 }
--- IntentKt/app/src/main/java/com/example/intentkt/SubActivity.kt
+++ IntentKt/app/src/main/java/com/example/intentkt/SubActivity.kt
@@ -2,10 +2,25 @@ package com.example.intentkt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.intentkt.databinding.ActivityMainBinding
+import com.example.intentkt.databinding.ActivitySubBinding
 
 class SubActivity : AppCompatActivity() {
+
+    private var mBinding : ActivitySubBinding? = null
+
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sub)
+
+        mBinding = ActivitySubBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        if(intent.hasExtra("msg")){ // intent안에 msg라는 객체가 있다면?
+            // Sub Activity에 존재하는 textView에다가 HelloWorld가 넘겨져 온다.
+            binding.tvGetMsg.setText(intent.getStringExtra("msg"))
+        }
     }
 }
