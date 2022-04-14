10c7eca3d6ad44e75dce17b0c13cfa4f1babe7e6
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-04-14 13:46:02 +0900

    Intent를 활용한 화면 이동

--- IntentKt/app/src/main/java/com/example/intentkt/MainActivity.kt
+++ IntentKt/app/src/main/java/com/example/intentkt/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.intentkt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.intentkt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var mBinding : ActivityMainBinding? = null
+
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.btnA.setOnClickListener{
+            // var : 변수 / val : 자바에서는 final 값이 변경되지 못하는 변수(상수)
+            val intent = Intent(this,SubActivity::class.java) // 다음 화면으로 이동하기 위한 Intent객체 생성
+
+            startActivity(intent)
+        }
     }
 }
