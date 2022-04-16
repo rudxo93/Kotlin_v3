625374d2bfa45621d717eba48b7736fac9c150e8
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-04-16 16:57:56 +0900

    RecyclerViewKt 프로젝트 생성

---
+++ RecyclerViewKt/app/src/main/java/com/example/recyclerviewkt/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.recyclerviewkt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.recyclerviewkt.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var mBinding : ActivityMainBinding? = null
+    private val binding get() = mBinding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
