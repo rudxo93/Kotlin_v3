9a9a74e9304c53068b20d722db1657632b12f917
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-04-17 11:14:00 +0900

    프레그먼트를 사용해서 간단하게 화면 전환하기

--- FragmentKt/app/src/main/java/com/example/fragmentkt/MainActivity.kt
+++ FragmentKt/app/src/main/java/com/example/fragmentkt/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.fragmentkt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fragmentkt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var mBinding: ActivityMainBinding? = null
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setFrag(0)
+
+        binding.btnFragment1.setOnClickListener {
+            setFrag(0)
+        }
+
+        binding.btnFragment2.setOnClickListener {
+            setFrag(1)
+        }
+
+        binding.btnFragment3.setOnClickListener {
+            setFrag(2)
+        }
+    }
+
+    private fun setFrag(fragNum : Int ) {
+        val ft = supportFragmentManager.beginTransaction()
+        when(fragNum) {
+            0 -> {
+                ft.replace(R.id.main_frame, Fragment1()).commit() // 교체하고 저장
+            }
+            1 -> {
+                ft.replace(R.id.main_frame, Fragment2()).commit()
+            }
+            2 -> {
+                ft.replace(R.id.main_frame, Fragment3()).commit()
+            }
+        }
     }
 }
