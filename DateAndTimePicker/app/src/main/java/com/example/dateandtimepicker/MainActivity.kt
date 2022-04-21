de6cafbd8b57eefa4a09fc52c732b3d28f9b884b
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-04-21 10:31:28 +0900

    DatePicker 구현해보기

--- DateAndTimePicker/app/src/main/java/com/example/dateandtimepicker/MainActivity.kt
+++ DateAndTimePicker/app/src/main/java/com/example/dateandtimepicker/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.dateandtimepicker
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.dateandtimepicker.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var tvDatePicker : TextView
+    private lateinit var btnDatePicker : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvDatePicker = findViewById(R.id.tvDate)
+        btnDatePicker = findViewById(R.id.btnDatePicker)
+
+        val myCalendar = Calendar.getInstance()
+
+        val datePicker = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            myCalendar.set(Calendar.YEAR, year)
+            myCalendar.set(Calendar.MONTH, month)
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateLable(myCalendar)
+        }
+
+        btnDatePicker.setOnClickListener {
+            DatePickerDialog(this, datePicker, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)).show()
+        }
+
+    }
+
+    private fun updateLable(myCalendar: Calendar) {
+        val myFormat = "dd-MM-yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.UK)
+        tvDatePicker.setText(sdf.format(myCalendar.time))
     }
 }
